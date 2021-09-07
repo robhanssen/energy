d2bb940fc25e50c0c67fe85a743d41ec7c4fa698
Rob Hanssen <rob.hanssen@outlook.com>
2021-09-07 11:11:21 -0400

    new solver fit

--- energy-by-temperature.r
+++ energy-by-temperature.r
@@ -35,16 +35,30 @@ energy_temperature <-
 #         aes(x = temp_f, y = use) + 
 #         geom_point()
 
+
+poweruse = energy_temperature$use
+temp = energy_temperature$temp
+
+k <- 10
+fm0 <- lm(poweruse ~ temp, subset = seq(to = length(temp), length = k))
+st <- list(C = mean(head(poweruse, k)), A = coef(fm0)[[1]], B = coef(fm0)[[2]])
+
+fm <- nls(poweruse ~ pmax(C, A + B * temp + D*temp^2), start = st)
+fm
+
+nlsmod <- fm %>% broom::augment(newdata = tibble(temp = -5:max(temp))) 
+
 usemod <- lm(use ~ temp, data = energy_temperature %>% filter(temp > 18))
 usemod_flat <- lm(use ~ temp, data = energy_temperature %>% filter(temp < 18))
 
 usepred <- usemod %>% broom::augment(newdata = tibble(temp = -5:40)) 
-usepred_flat <- usemod_flat %>% broom::augment(newdata = tibble(temp = -5:40)) 
+usepred_flat <- usemod_flat %>% broom::augment(newdata = tibble(temp = -5:max(nlsmod$temp))) 
 
 use <- full_join(usepred, usepred_flat, by = "temp") %>% 
         group_by(temp) %>%
         mutate(.fitted = max(.fitted.x, .fitted.y))
 
+fm %>% tidy()
 
 usemod %>% broom::glance()
 usemod %>% broom::tidy()
@@ -52,7 +66,7 @@ usemod %>% broom::tidy()
 usemod_flat %>% broom::glance()
 usemod_flat %>% broom::tidy()
 
-
+with(as.list(coef(fm)), (C - A)/B)
 
 energy_temperature %>%
     ggplot + 
@@ -61,4 +75,7 @@ energy_temperature %>%
             y = "Use per houre (kWh)") +
         geom_point(alpha = .8) + 
         scale_color_gradient2(low = "blue", high = "red", mid = "green", midpoint = 15) + 
-        geom_point(data = use, aes(y=.fitted), color = "red", size = 2) 
+        #geom_point(data = use, aes(y=.fitted), color = "red", size = 2) +
+        geom_line(data = nlsmod, aes(y=.fitted), color = "darkgreen", size = 2)
+
+ggsave("graphs/poweruse-by-hour.png")
