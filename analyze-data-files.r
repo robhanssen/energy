da680a98ac94663608fb707d75e16d25f9e66a8f
Rob Hanssen <rob.hanssen@outlook.com>
2022-04-12 15:21:37 -0400

    linting and read-updates

--- analyze-data-files.r
+++ analyze-data-files.r
@@ -2,23 +2,30 @@ library(tidyverse)
 library(lubridate)
 library(xml2)
 
-rawdatafiles <- list.files(path = "source/",
-                        pattern = "*.xml",
-                        full.names = TRUE)
+analyze_data_file <- function(f) {
 
-for (d in rawdatafiles) {
+        rawdata <- xml2::read_xml(f)
 
-        rawdata <- read_xml(d)
-
-        time <- xml_find_all(rawdata, "//espi:start") %>%
+        time <- xml2::xml_find_all(rawdata, "//espi:start") %>%
                         as_list() %>%
                         unlist() %>%
                         as.numeric()
 
         time <- time[2:length(time)]
 
-        earliest <- as_datetime(min(time))
-        latest <- as_datetime(max(time))
-        msg <- paste(d, earliest, latest, length(time))
-        print(msg)
+        earliest <- lubridate::as_datetime(min(time))
+        latest <- lubridate::as_datetime(max(time))
+
+        tibble::tibble(filename = f, earliest_time = earliest, latest_time = latest) %>%
+        mutate(across(ends_with("time"), ~format(.x, format = "%b %d, %Y")))
 }
+
+map_df(
+        list.files(
+                path = "source/",
+                pattern = "*.xml",
+                full.names = TRUE
+        ),
+        ~ analyze_data_file(.x)
+) %>%
+        knitr::kable()
--- energy.r
+++ energy.r
@@ -8,30 +8,33 @@ this_year <- year(today())
 datadir <- "./data-by-year"
 
 energy <- list.files(path = datadir, full.names = TRUE) %>%
-                map_df(~ read_csv(.))
+        map_df(~ read_csv(.))
 
 byday <- energy %>%
-            group_by(date) %>%
-            summarize(use = sum(energy),
-                      year = year,
-                      month = factor(month),
-                      day = day
-                        )
+        group_by(date) %>%
+        summarize(
+                use = sum(energy),
+                year = year,
+                month = factor(month),
+                day = day
+        )
 
 model <- lm(use ~ month + day, data = byday)
 dt <- model %>%
         broom::augment(interval = "confidence") %>%
         bind_cols(date = byday$date)
 
-byday %>% ggplot +
-            aes(x = date, y = use) +
-            geom_point() +
-            scale_x_date(date_breaks = "3 months", date_labels = "%B %d") +
-            geom_line(data = dt, aes(y = .fitted)) +
-            geom_ribbon(data = dt,
-                        aes(y = .fitted, ymax = .upper, ymin = .lower),
-                        fill = "red",
-                        alpha = .5)
+byday %>% ggplot() +
+        aes(x = date, y = use) +
+        geom_point() +
+        scale_x_date(date_breaks = "3 months", date_labels = "%B %d") +
+        geom_line(data = dt, aes(y = .fitted)) +
+        geom_ribbon(
+                data = dt,
+                aes(y = .fitted, ymax = .upper, ymin = .lower),
+                fill = "red",
+                alpha = .5
+        )
 # by month
 
 bymonth <-
@@ -55,27 +58,40 @@ bymonth_thisyear <-
 
 monthgraph <-
         bymonth %>%
-        ggplot +
+        ggplot() +
         aes(x = monthname, y = energy, group = FALSE) +
         # geom_line() +
         geom_ribbon(aes(ymin = 0, ymax = energy), alpha = .5) +
-        geom_line(data = bymonth_thisyear, aes(group = FALSE), lty = 2, size = 1) +
+        geom_line(
+                data = bymonth_thisyear,
+                aes(group = FALSE),
+                lty = 2,
+                size = 1
+        ) +
         geom_point(data = bymonth_thisyear) +
-        labs(x = "Month",
-             y = "Average monthly energy use (in kWh)",
-             title = "Comparison of historical vs current monthly energy use") +
+        labs(
+                x = "Month",
+                y = "Average monthly energy use (in kWh)",
+                title = "Comparison of historical vs current monthly energy use"
+        ) +
         scale_y_continuous(labels = scales::comma_format())
 
 # cumulative graph
 cumgraph <-
         bymonth %>%
-        ggplot +
+        ggplot() +
         aes(x = monthname, y = totaluse, group = FALSE) +
         geom_ribbon(aes(ymin = 0, ymax = totaluse), alpha = .5) +
-        geom_line(data = bymonth_thisyear, aes(group = FALSE), lty = 2, size = 1) +
+        geom_line(
+                data = bymonth_thisyear,
+                aes(group = FALSE),
+                lty = 2, size = 1
+        ) +
         geom_point(data = bymonth_thisyear) +
-        labs(x = "Month",
-             y = "Average monthly energy use (in kWh)") +
+        labs(
+                x = "Month",
+                y = "Average monthly energy use (in kWh)"
+        ) +
         scale_y_continuous(labels = scales::comma_format())
 
 plot <- monthgraph + cumgraph
@@ -85,27 +101,33 @@ ggsave("graphs/monthcomparison.png", width = 12, height = 6, plot = plot)
 # this month
 
 monthsago <- function(x) {
-                      lubridate::floor_date(lubridate::today() - months(x),
-                                            "month"
-                                            )
-                }
+        lubridate::floor_date(
+                lubridate::today() - months(x),
+                "month"
+        )
+}
 
 plotrange <- 6
 
 byday %>%
         filter(date > monthsago(plotrange)) %>%
-        ggplot +
-            aes(x = date, y = use) +
-            geom_point() +
-            scale_x_date(date_breaks = "3 months",
-                          date_labels = "%B %d",
-                          limits = c(monthsago(plotrange), today())) +
-            geom_line(data = dt, aes(y = .fitted)) +
-            geom_ribbon(data = dt, aes(y = .fitted,
-                                      ymax = .upper,
-                                      ymin = .lower),
-                        fill = "red", alpha = .5
-                        )
+        ggplot() +
+        aes(x = date, y = use) +
+        geom_point() +
+        scale_x_date(
+                date_breaks = "3 months",
+                date_labels = "%B %d",
+                limits = c(monthsago(plotrange), today())
+        ) +
+        geom_line(data = dt, aes(y = .fitted)) +
+        geom_ribbon(
+                data = dt, aes(
+                        y = .fitted,
+                        ymax = .upper,
+                        ymin = .lower
+                ),
+                fill = "red", alpha = .5
+        )
 
 #
 #  What is the average daily and monthly use?
@@ -113,72 +135,84 @@ byday %>%
 
 # daily use
 dailyuse <- energy %>%
-              group_by(date) %>%
-              summarize(dailyuse = sum(energy)) %>%
-              mutate(runningdate = month(date) * 100 + day(date))
+        group_by(date) %>%
+        summarize(dailyuse = sum(energy)) %>%
+        mutate(runningdate = month(date) * 100 + day(date))
 
 # average use on a single day in the year, and rollmean that over 14 days
 averagedailyuse <- dailyuse %>%
-                      group_by(runningdate) %>%
-                      summarize(dayuse = mean(dailyuse))
+        group_by(runningdate) %>%
+        summarize(dayuse = mean(dailyuse))
 
-rollingdailyuse <- bind_rows(tail(averagedailyuse, 6),
-                            averagedailyuse,
-                            head(averagedailyuse, 7)) %>%
-                    mutate(rollingmean = zoo::rollmean(dayuse,
-                                                        14,
-                                                        na.pad = TRUE)
-                                                        ) %>%
-                    filter(!is.na(rollingmean))
+rollingdailyuse <- bind_rows(
+        tail(averagedailyuse, 6),
+        averagedailyuse,
+        head(averagedailyuse, 7)
+) %>%
+        mutate(rollingmean = zoo::rollmean(dayuse,
+                14,
+                na.pad = TRUE
+        )) %>%
+        filter(!is.na(rollingmean))
 
 thisyear <- inner_join(dailyuse, rollingdailyuse) %>%
-            filter(date >= floor_date(today(), "1 year")) %>%
-            mutate(cumuse = cumsum(dailyuse),
-                   cumeaveuse = cumsum(rollingmean)
-                   )
+        filter(date >= floor_date(today(), "1 year")) %>%
+        mutate(
+                cumuse = cumsum(dailyuse),
+                cumeaveuse = cumsum(rollingmean)
+        )
 
 thisyear %>%
-            ggplot() +
-            aes(x = date, y = dailyuse) +
-            geom_line(aes(y = rollingmean), color = "red", lty = 1, size = 1) +
-            geom_line(aes(y = zoo::rollmean(dailyuse, 7, na.pad = TRUE))) +
-            expand_limits(y = 0, x = ceiling_date(today(), "1 month")) +
-            theme_light() +
-            labs(x = paste0("Date (in ", this_year, ")"),
-                   y = "Daily energy use (in kWh)",
-                   caption = "Red line is long-term average\nBlack line is current 14-days rolling average")
+        ggplot() +
+        aes(x = date, y = dailyuse) +
+        geom_line(aes(y = rollingmean), color = "red", lty = 1, size = 1) +
+        geom_line(aes(y = zoo::rollmean(dailyuse, 7, na.pad = TRUE))) +
+        expand_limits(y = 0, x = ceiling_date(today(), "1 month")) +
+        theme_light() +
+        labs(
+                x = paste0("Date (in ", this_year, ")"),
+                y = "Daily energy use (in kWh)",
+                caption = "Red line is long-term average\nBlack line is current 14-days rolling average" # nolint
+        )
 
 
 ggsave("graphs/thisyearcomparedtoalltime.png", width = 8, height = 6)
 
 thisyear %>%
-            ggplot +
-            aes(x = date, y = cumuse) +
-            geom_point(color = "black", fill = "black", alpha = .2) +
-            geom_line(aes(y = cumeaveuse)) +
-            scale_x_date(date_breaks = "2 months", date_label = "%b %Y")
+        ggplot() +
+        aes(x = date, y = cumuse) +
+        geom_point(color = "black", fill = "black", alpha = .2) +
+        geom_line(aes(y = cumeaveuse)) +
+        scale_x_date(date_breaks = "2 months", date_label = "%b %Y")
 
 #
 # cumulative use with current year highlighted
-#            
+#
 
-this_year = year(today())
+this_year <- year(today())
 
 cumulative_use <-
-        energy %>% 
+        energy %>%
         group_by(date) %>%
-        summarize(energy = sum(energy), 
-                  .groups = "drop") %>%
+        summarize(
+                energy = sum(energy),
+                .groups = "drop"
+        ) %>%
         mutate(year = year(date)) %>%
         group_by(year) %>%
-        mutate(yearuse = cumsum(energy), 
-               color = case_when(year == year(today()) ~ "red",
-                                 TRUE ~ "gray50"), 
-               scaled_date = date + years(this_year - year)
-                ) %>%
+        mutate(
+                yearuse = cumsum(energy),
+                color = case_when(
+                        year == year(today()) ~ "red",
+                        TRUE ~ "gray50"
+                ),
+                scaled_date = date + years(this_year - year)
+        ) %>%
         ungroup() %>%
-        mutate(yearuse = case_when(year == 2019 ~ 9313 - 5580 + yearuse,
-                                   TRUE ~ yearuse))
+        mutate(yearuse = case_when(
+                year == 2019 ~ 9313 - 5580 + yearuse,
+                TRUE ~ yearuse
+        ))
 
 min_year <- min(cumulative_use$year)
 max_year <- max(cumulative_use$year)
@@ -188,31 +222,38 @@ linetypes <- c(rep("dotted", length(yearcolorrange) - 2), "dashed", "solid")
 linsizes <- c(rep(0.5, length(yearcolorrange) - 2), 0.75, 1.5)
 
 cumulative_use %>%
-        ggplot +
-        aes(x = scaled_date,
-            y = yearuse,
-            color = factor(year, levels = min_year:max_year),
-            size = factor(year, levels = min_year:max_year),
-            linetype = factor(year, levels = min_year:max_year)) +
-        geom_line() + 
+        ggplot() +
+        aes(
+                x = scaled_date,
+                y = yearuse,
+                color = factor(year, levels = min_year:max_year),
+                size = factor(year, levels = min_year:max_year),
+                linetype = factor(year, levels = min_year:max_year)
+        ) +
+        geom_line() +
         scale_color_manual(values = yearcolor) +
         scale_linetype_manual(values = linetypes) +
         scale_size_manual(values = linsizes) +
-        scale_x_date(date_labels = "%b") + 
-        scale_y_continuous(labels = scales::comma_format(),
-                           breaks = 2000 * 0:10) +
-        labs(x = "Date",
-             y = "Cumulative annual use (in kWh)",
-             title = "Comparison of cumulative use of electricity",
-             caption = paste0("Red line: ", 
-                              this_year,
-                              "\nBlack line: ",
-                              this_year - 1,
-                              "\nGray lines: ",
-                              min_year,
-                              "-",
-                              max_year - 2)
-             ) +
+        scale_x_date(date_labels = "%b") +
+        scale_y_continuous(
+                labels = scales::comma_format(),
+                breaks = 2000 * 0:10
+        ) +
+        labs(
+                x = "Date",
+                y = "Cumulative annual use (in kWh)",
+                title = "Comparison of cumulative use of electricity",
+                caption = paste0(
+                        "Red line: ",
+                        this_year,
+                        "\nBlack line: ",
+                        this_year - 1,
+                        "\nGray lines: ",
+                        min_year,
+                        "-",
+                        max_year - 2
+                )
+        ) +
         theme(legend.position = "none")
 
 ggsave("graphs/cumulative-use.png", width = 6, height = 6)
